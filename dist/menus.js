23b78e610b9066d6c520c0b1b25d6c975ffdaadf
studiosmith-cordell <157119820+studiosmith-cordell@users.noreply.github.com>
2024-02-06 16:34:41 +0000

    vegan update

--- dist/menus.js
+++ dist/menus.js
@@ -783,9 +783,14 @@
     let filter = '[data-filter-checkbox="' + button + '"]';
     if (button === "Vegetarian") {
       let vegetarian = $('[data-filter-checkbox="Vegetarian"]').is(":checked");
-      if (!vegetarian) {
-        $('[data-filter-checkbox="Vegan"]')[0].checked = true;
-        $('[data-filter-button="Vegan"]').addClass("filter__btn--active");
+      let vegan = $('[data-filter-checkbox="Vegan"]').is(":checked");
+      if (vegetarian) {
+        $('[data-filter-checkbox="Vegan"]')[0].checked = false;
+        $('[data-filter-button="Vegan"]').removeClass("filter__btn--active");
+      }
+      if (vegan) {
+        $('[data-filter-checkbox="Vegetarian"]')[0].checked = false;
+        $('[data-filter-button="Vegetarian"]').removeClass("filter__btn--active");
       }
     }
     ;
