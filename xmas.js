b3abf59bdee4d946c1860cccd46e44517325aafa
studiosmith-cordell <157119820+studiosmith-cordell@users.noreply.github.com>
2026-06-08 17:02:29 +0100

    snow density mobile

--- xmas.js
+++ xmas.js
@@ -1,5 +1,5 @@
-const NUMBER_OF_SNOWFLAKES = 150,
-	MAX_SNOWFLAKE_SIZE = 5,
+const NUMBER_OF_SNOWFLAKES = window.innerWidth > 640 ? 150 : 75,
+	MAX_SNOWFLAKE_SIZE = window.innerWidth > 640 ? 5 : 3,
 	MAX_SNOWFLAKE_SPEED = 1,
 	SNOWFLAKE_COLOUR = '#fbf5ee',
 	snowflakes = [];
